75098f713b761309cdb0657d39e9478e373f4000
Aditya26Das <aditya26.das@gmail.com>
2024-06-21 13:09:41 +0530

    Final Commit

--- server.js
+++ server.js
@@ -4,10 +4,20 @@ import errorHandler from "./middlewares/error.js";
 import router from "./routes/post.js";
 import logger from "./middlewares/logger.js";
 import notFound from "./middlewares/notFound.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+//Get the Directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
 
 const port = process.env.PORT || 8000;
 const app = express();
 configDotenv("./.env");
+
+//Setup a Static Folder
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
